81d3e057bd9b4dc00f9d043e6b487621c7284563
Ryan Clemente <rclemente@zendesk.com>
2018-02-01 17:32:32 +0800

    adding onchange

---
+++ src/containers/search_bar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    // this sets the context of onInputChange
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({ term: event.target.value });
+  }
+
+  render() {
+    return (
+      <form className="input-group">
+        <input
+          placeholder="Get a five-day forecarst in your favorite cities"
+          className="fom-control"
+          value={this.state.term}
+          onChange={this.onInputChange} />
+        <span className="input-group-append">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    );
+  }
+}
