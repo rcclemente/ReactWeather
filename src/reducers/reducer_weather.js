b42158542c86dd869c10fad8f176ad7fcec0b9ae
Ryan Clemente <rclemente@zendesk.com>
2018-03-25 17:55:05 +0800

    adding reducer_weather

---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,10 @@
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  console.log('Action recieved: ', action);
+  switch (action.type) {
+    case FETCH_WEATHER :
+      return [ action.payload.data ];
+  }
+  return state;
+}
