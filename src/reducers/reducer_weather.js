b4142f4f27bf49cf33073b9882f5e48bfbc894b4
Ryan Clemente <rclemente@zendesk.com>
2018-04-03 14:22:26 +0800

    adding charts

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,7 +1,7 @@
 import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action) {
-  console.log('Action recieved: ', action);
+  // console.log('Action recieved: ', action);
   switch (action.type) {
   case FETCH_WEATHER :
     return [ action.payload.data, ...state ];
